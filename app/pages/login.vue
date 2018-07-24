b445e74e3e8d9f840044f9c0d23af19ef9457903
Dendi Handian <handian.dendi@gmail.com>
2018-07-24 22:27:00 +0700

    add login page

---
+++ app/pages/login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    email: <input v-model="email"/><br>
+    password: <input v-model="password" type="password"/><br>
+    <button @click="login">Login</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    email: null,
+    password: null
+  }),
+  methods: {
+    login() {
+      this.$axios.post('auth/login', {
+        email: this.email,
+        password: this.password
+      }).then(response => {
+        localStorage.setItem("api_token", response.data.api_token);
+      })
+    }
+  }
+}
+</script>
